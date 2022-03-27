49168122f740809ad45c3d337fc7be2f43a38af4
Jonas <jonasmirandaa21@gmail.com>
2022-03-27 13:44:24 -0300

    feat: Criar compontente button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Button from './components/button/button';
 
 function App() {
   return (
     <div className="App">
-      
+      <Button/>
     </div>
   );
 }
