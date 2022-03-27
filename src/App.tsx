48aaadc3a4b0766963f97444615a7d8ab2216eed
Jonas <jonasmirandaa21@gmail.com>
2022-03-27 14:24:30 -0300

    feat: Criar componente list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Form from './components/form/form';
+import List from './components/list/list';
 
 function App() {
   return (
     <div className="App">
       <Form/>
+      <List/>
     </div>
   );
 }
