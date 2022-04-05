39befebdb9c016429d5b83e6310298e10678330a
Jonas <jonasmirandaa21@gmail.com>
2022-04-05 14:27:07 -0300

    feat: Adicionar componentes criados no app

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Form from "../components/form/form";
 import List from "../components/list/list";
+import Cronometro from "../components/cronometro/cronometro";
 import style from "./App.module.scss";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className={style.AppStyle}>
       <Form />
       <List />
+      <Cronometro />
     </div>
   );
 }
