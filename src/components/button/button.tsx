f3ccbf0770fba6680589d0d40037d8a174429fa4
Jonas <jonasmirandaa21@gmail.com>
2022-08-04 13:08:06 -0300

    refactor: update class component to function component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import style from "./button.module.scss";
 
-class Button extends React.Component<{
+interface Props {
   type?: "button" | "submit" | "reset" | undefined;
   onClick?: () => void;
-}> {
-  render() {
-    const { type = "button", onClick } = this.props;
-    return (
-      <button onClick={onClick} type={type} className={style.botao}>
-        {this.props.children}
-      </button>
-    );
-  }
+  children?: React.ReactNode;
+}
+
+function Button({ onClick, type, children }: Props) {
+  return (
+    <button onClick={onClick} type={type} className={style.botao}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,68 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { Itarefa } from "../../types/tarefa";
 import Button from "../button/button";
 import style from "./form.module.scss";
 import { v4 as uuidv4 } from "uuid";
 
-class Form extends React.Component<{
+interface Props {
   setTarefas: React.Dispatch<React.SetStateAction<Itarefa[]>>;
-}> {
-  state = {
-    tarefa: "",
-    tempo: "00:00",
-  };
+}
 
-  adicionarTarefa(evento: React.FormEvent) {
+function Form({ setTarefas }: Props) {
+  const [tarefa, setTarefa] = useState("");
+  const [tempo, setTempo] = useState("00:00");
+
+  function adicionarTarefa(evento: React.FormEvent) {
     evento.preventDefault();
-    this.props.setTarefas((tarefasAntigas) => [
+    setTarefas((tarefasAntigas) => [
       ...tarefasAntigas,
-      { ...this.state, selecionado: false, completado: false, id: uuidv4() },
+      { tarefa, tempo, selecionado: false, completado: false, id: uuidv4() },
     ]);
-    this.setState({
-      tarefa: "",
-      tempo: "00:00",
-    });
+    setTarefa("");
+    setTempo("00:00");
   }
-  render() {
-    return (
-      <form
-        className={style.novaTarefa}
-        onSubmit={this.adicionarTarefa.bind(this)}
-      >
-        <div className={style.inputContainer}>
-          <label htmlFor="tarefa">Adicione um novo estudo</label>
-          <input
-            type="text"
-            name="tarefa"
-            value={this.state.tarefa}
-            onChange={(evento) =>
-              this.setState({ ...this.state, tarefa: evento.target.value })
-            }
-            id="tarefa"
-            placeholder="O que voce quer estudar"
-            required
-          />
-        </div>
-        <div className={style.inputContainer}>
-          <label htmlFor="tempo">Tempo</label>
-          <input
-            type="time"
-            step="1"
-            name="tempo"
-            value={this.state.tempo}
-            onChange={(evento) =>
-              this.setState({ ...this.state, tempo: evento.target.value })
-            }
-            id="tempo"
-            min="00:00:00"
-            max="01:30:00"
-            required
-          />
-        </div>
-        <Button type="submit">Adicionar</Button>
-      </form>
-    );
-  }
+
+  return (
+    <form className={style.novaTarefa} onSubmit={adicionarTarefa}>
+      <div className={style.inputContainer}>
+        <label htmlFor="tarefa">Adicione um novo estudo</label>
+        <input
+          type="text"
+          name="tarefa"
+          value={tarefa}
+          onChange={(evento) => setTarefa(evento.target.value)}
+          id="tarefa"
+          placeholder="O que voce quer estudar"
+          required
+        />
+      </div>
+      <div className={style.inputContainer}>
+        <label htmlFor="tempo">Tempo</label>
+        <input
+          type="time"
+          step="1"
+          name="tempo"
+          value={tempo}
+          onChange={(evento) => setTempo(evento.target.value)}
+          id="tempo"
+          min="00:00:00"
+          max="01:30:00"
+          required
+        />
+      </div>
+      <Button type="submit">Adicionar</Button>
+    </form>
+  );
 }
 
 export default Form;
