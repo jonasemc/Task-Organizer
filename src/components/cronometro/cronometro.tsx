cfaebc56a28d68386982fd6eb80376273e87a1bb
Jonas <jonasmirandaa21@gmail.com>
2022-08-04 12:37:49 -0300

    feat: create counter logic for the clock

--- src/components/cronometro/cronometro.tsx
+++ src/components/cronometro/cronometro.tsx
@@ -1,17 +1,37 @@
 import Button from "../button/button";
 import Relogio from "./relogio/relogio";
 import style from "./cronometro.module.scss";
-import { tempoParaSegundos } from "../../common/utils/date";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { Itarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
 
-export default function Cronometro() {
-  console.log("conversao:", tempoParaSegundos("01:01:01"));
+interface Props {
+  selecionado: Itarefa | undefined;
+}
+
+export default function Cronometro({ selecionado }: Props) {
+  const [tempo, setTempo] = useState<number>();
+
+  useEffect(() => {
+    selecionado?.tempo && setTempo(tempoParaSegundos(selecionado.tempo));
+  }, [selecionado]);
+
+  function regressiva(contador: number = 0) {
+    setTimeout(() => {
+      if (contador > 0) {
+        setTempo(contador - 1);
+        return regressiva(contador - 1);
+      }
+    }, 1000);
+  }
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronometro</p>
       <div className={style.relogioWrapper}>
-        <Relogio />
+        <Relogio tempo={tempo} />
       </div>
-      <Button> Comecar!</Button>
+      <Button onClick={() => regressiva(tempo)}> Comecar!</Button>
     </div>
   );
 }
